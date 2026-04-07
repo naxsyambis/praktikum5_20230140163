14ce1ec9fb81234bae5511efe7df42a0dc0c176f
naxsyambis <naxsyambis@gmail.com>
2026-04-07 14:10:01 +0700

    feat: add home navigation and login validation logic

--- src/main/java/com/praktikum5/deploy_P5_163/controller/UserController.java
+++ src/main/java/com/praktikum5/deploy_P5_163/controller/UserController.java
@@ -1,1 +1,38 @@
 package com.praktikum5.deploy_P5_163.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+	private final String USERNAME = "admin";
+	private final String PASSWORD = "20230140163";
+
+	@GetMapping("/")
+	public String loginPage() {
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(
+		@RequestParam String username,
+		@RequestParam String password,
+		Model model) {
+
+		if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+			return "home";
+		} else {
+			model.addAttribute("error", "username atau password");
+		}
+		return "login";
+	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
